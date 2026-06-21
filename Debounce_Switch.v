037a0efc38b2455cf9597f8977deb73a03e07541
George Frank Archibong <georgefrankarchibong@gmail.com>
2026-06-21 11:24:33 -0500

    Implement debounce switch module in Verilog

---
+++ Debounce_Switch.v
@@ -0,0 +1,24 @@
+module Debounce_Switch
+  #(parameter c_DEBOUNCE_LIMIT = 250000)  // ~10ms @ 25MHz
+  (input  i_Clk,
+   input  i_Switch,
+   output o_Switch);
+
+  reg r_State = 1'b0;
+  reg [17:0] r_Count = 0;
+
+  always @(posedge i_Clk)
+  begin
+    if (i_Switch !== r_State && r_Count < c_DEBOUNCE_LIMIT)
+      r_Count <= r_Count + 1;
+    else if (r_Count == c_DEBOUNCE_LIMIT)
+    begin
+      r_State <= i_Switch;
+      r_Count <= 0;
+    end
+    else
+      r_Count <= 0;
+  end
+
+  assign o_Switch = r_State;
+endmodule
